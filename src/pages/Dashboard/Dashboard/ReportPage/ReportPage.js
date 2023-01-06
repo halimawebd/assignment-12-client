c8c07878d5431093ac7d16bade45b4935f6e1d6e
halimawebd <halimawebd@gmail.com>
2023-01-06 17:41:05 +0600

    ReportPage part

--- src/pages/Dashboard/Dashboard/ReportPage/ReportPage.js
+++ src/pages/Dashboard/Dashboard/ReportPage/ReportPage.js
@@ -1,23 +1,23 @@
-// import { useQuery } from '@tanstack/react-query';
-// import React from 'react';
-// import ReportCard from './ReportCard';
+import { useQuery } from '@tanstack/react-query';
+import React from 'react';
+import ReportCard from './ReportCard';
 
-// const ReportPage = () => {
-//     const { data: userReport, refetch } = useQuery({
-//         queryKey: ["report"],
-//         queryFn: async () => {
-//           const res = await fetch("http://localhost:4000/report");
-//           const data = await res.json();
-//           return data;
-//         },
-//       });
-//     //   console.log(userReport)
+const ReportPage = () => {
+    const { data: userReport, refetch } = useQuery({
+        queryKey: ["report"],
+        queryFn: async () => {
+          const res = await fetch("http://localhost:4000/report");
+          const data = await res.json();
+          return data;
+        },
+      });
+    //   console.log(userReport)
       
-//     return (
-//         <div className='w-[800px] mx-auto'>
-//             {userReport?.map(rep => <ReportCard report={rep}/>)}
-//         </div>
-//     );
-// };
+    return (
+        <div className='w-[800px] mx-auto'>
+            {userReport?.map(rep => <ReportCard report={rep}/>)}
+        </div>
+    );
+};
 
-// export default ReportPage;
+export default ReportPage;
