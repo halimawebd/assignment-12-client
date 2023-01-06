3bc106f2b1734d50d5278b722be9340e6e2b2178
halimawebd <halimawebd@gmail.com>
2023-01-06 16:20:13 +0600

    loader part

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,12 +1,12 @@
-// import React from 'react';
+import React from 'react';
 
-// const Loader = () => {
-//   return (
-//     <div>
-//       <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin dark:border-violet-400"></div>
+const Loader = () => {
+  return (
+    <div>
+      <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin dark:border-violet-400"></div>
    
-//     </div>
-//   );
-// };
+    </div>
+  );
+};
 
-// export default Loader;
+export default Loader;
