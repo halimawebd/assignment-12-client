f7487459bba8c9ed586c8ea66ccf3461012a56a8
halimawebd <halimawebd@gmail.com>
2023-01-06 16:52:06 +0600

    sellerRoutes part

--- src/Routes/SellerRoutes/SellerRoutes.js
+++ src/Routes/SellerRoutes/SellerRoutes.js
@@ -1,27 +1,27 @@
-// import React, { useContext } from 'react';
-// import { Navigate, useLocation } from 'react-router-dom';
-// import { AuthContext } from '../../context/AuthProvider';
-// import useSeller from '../../customHooks/useSeller';
+import React, { useContext } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import { AuthContext } from '../../context/AuthProvider';
+import useSeller from '../../customHooks/useSeller';
 
 
 
 
 
-// const SellerRoutes = ({ children }) => {
-//     const { user, loading } = useContext(AuthContext);
-//     const [isSeller, setIsSellerLoading] = useSeller(user?.email);
-//     const location = useLocation();
+const SellerRoutes = ({ children }) => {
+    const { user, loading } = useContext(AuthContext);
+    const [isSeller, setIsSellerLoading] = useSeller(user?.email);
+    const location = useLocation();
 
-//     if (loading || setIsSellerLoading) {
-//         return <p>Loading...</p>
-//     }
+    if (loading || setIsSellerLoading) {
+        return <p>Loading...</p>
+    }
 
-//     if (user && isSeller) {
-//         return children;
-//     }
+    if (user && isSeller) {
+        return children;
+    }
 
-//     return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
+    return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
 
-// };
+};
 
-// export default SellerRoutes;
+export default SellerRoutes;
